0d3315af153a24c41acd48bbc9a9cb9f2114e791
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-06 17:37:09 -0700

    Create game_nights controller
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>
    Co-authored-by: Phil DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>

---
+++ app/controllers/game_nights_controller.rb
@@ -0,0 +1,13 @@
+class GameNightsController < ApplicationController
+  def new
+    @user = UserFacade.user_info('200')
+  end
+
+  def create
+    game_night = GameNightFacade.create_game_night(params)
+    redirect_to "/game-nights/#{game_night.id}"
+  end
+
+  def show
+  end
+end
