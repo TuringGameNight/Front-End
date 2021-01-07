5da765691487f4f73220411d879020c866ec772a
Kate Tester <katemorris@gmail.com>
2021-01-07 15:36:45 -0700

    Fixes some rubocop issues
    Co-authored-by: Greg Mitchell <67349568+GregJMitchell@users.noreply.github.com>

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -11,22 +11,22 @@ describe 'As an authenticated user' do
     it 'has a welcome message, a list of my game nights, a list of my games and a list of my friends' do
       visit dashboard_index_path
 
-      within ('#welcome-message') do
-        expect(page).to have_content("Welcome Jake!")
+      within('#welcome-message') do
+        expect(page).to have_content('Welcome Jake!')
       end
 
-      within ('#user-games') do
-        expect(page).to have_content("Dungeons & Dragons")
-        expect(page).to have_content("Pandemic")
+      within('#user-games') do
+        expect(page).to have_content('Dungeons & Dragons')
+        expect(page).to have_content('Pandemic')
       end
 
-      within ('#game-nights') do
-        expect(page).to have_content("Dwarves Rule 2021")
+      within('#game-nights') do
+        expect(page).to have_content('Dwarves Rule 2021')
       end
 
-      within ('#friends') do
-        expect(page).to have_content("Sean")
-        expect(page).to have_content("Robert")
+      within('#friends') do
+        expect(page).to have_content('Sean')
+        expect(page).to have_content('Robert')
       end
     end
   end
