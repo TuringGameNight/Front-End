4c85f3bac0b527f2645b0c0034a70f78337ecee1
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-06 21:43:27 -0700

    Edits user facade to map over changed json structure

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -1,8 +1,8 @@
 class UserFacade
   def self.user_info(user_id)
     json = UserService.user_info(user_id)
-    friends = json[:data][:relationships][:friends].map do |data|
-      Friend.new(data[1])
+    friends = json[:data][:relationships][:friends][:data].map do |data|
+      Friend.new(data)
     end
     user = User.new(json[:data])
     user.add_friends(friends)
