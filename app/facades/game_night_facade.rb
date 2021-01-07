9c28f830cdaeb1c2a932c10e2455607607098133
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-06 21:43:40 -0700

    Edits game_night facade to map over changed json structure

--- app/facades/game_night_facade.rb
+++ app/facades/game_night_facade.rb
@@ -2,8 +2,8 @@ class GameNightFacade
   def self.create_game_night(data)
     json = GameNightService.create_game_night(data)
     game_night = GameParty.new(json[:data])
-    friends = json[:data][:relationships][:friends].map do |data|
-      Friend.new(data[1])
+    friends = json[:data][:relationships][:friends][:data].map do |data|
+      Friend.new(data)
     end
     game_night.add_friends(friends)
     game_night
