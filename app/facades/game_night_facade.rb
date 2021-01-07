4037abb98395e6a0ea120089ee5110ce0be8bdae
Austin Aspaas <araspaas@live.com>
2021-01-06 21:18:15 -0700

    Add find_game_night method to game night facade
    Co-authored-by: Greg Mitchell <67349568+GregJMitchell@users.noreply.github.com>

--- app/facades/game_night_facade.rb
+++ app/facades/game_night_facade.rb
@@ -8,4 +8,18 @@ class GameNightFacade
     game_night.add_friends(friends)
     game_night
   end
+
+  def self.find_game_night(id)
+    json = GameNightService.find_game_night(id)
+    game_night = GameParty.new(json[:data])
+    friends = json[:data][:relationships][:friends].map do |data|
+      Friend.new(data[1])
+    end
+    games = json[:data][:relationships][:games].map do |data|
+      Game.new(data[1])
+    end
+    game_night.add_friends(friends)
+    game_night.add_games(games)
+    game_night
+  end
 end
