a77a3ae33d3e025d4d6e94a7bc92296a0340e9de
Austin Aspaas <araspaas@live.com>
2021-01-06 21:14:59 -0700

    Add find_game_night method to game night service
    Co-authored-by: Greg Mitchell <67349568+GregJMitchell@users.noreply.github.com>

--- app/services/game_night_service.rb
+++ app/services/game_night_service.rb
@@ -13,4 +13,10 @@ class GameNightService
 
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.find_game_night(id)
+    conn = Faraday.new("#{ENV['BACKEND_URL']}/api/v1/game-nights/#{id}")
+    response = conn.get
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
