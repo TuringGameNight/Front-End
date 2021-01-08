f80e189c1718b1d88e3bace9a7ac72bb5643005b
Kate Tester <katemorris@gmail.com>
2021-01-07 20:38:36 -0700

    Adds create functions with success and error messages
    Co-authored-by: Greg Mitchell <67349568+GregJMitchell@users.noreply.github.com>

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,8 +4,17 @@ class GamesController < ApplicationController
   end
 
   def create
-    GameFacade.create_game(params)
-    redirect_to dashboard_index_path
+    user = UserFacade.user_info('200')
+    response = GameFacade.create_game(params, user)
+    # need to pass user so backend can add the game to the user, not sure how
+    if response.status == 200
+      flash[:success] = 'Game created successfully and added to your shelf!'
+      redirect_to dashboard_index_path
+    else
+      # how do we deal with errors coming from the back end? This will need to change.
+      flash.now[:error] = 'Please fill in all required fields.'
+      render :new
+    end
   end
 
 end
