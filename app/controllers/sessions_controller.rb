0a8085b1f1191010d0c7dfedeacb942f5bc623cd
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-08 18:54:46 -0700

    Adds sessions controller with create action
    Co-authored-by: Shaunda Cunningham 
    <4582791+smcunning@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,15 @@ class SessionsController < ApplicationController
       user_data: request.env['omniauth.auth']
     }
 
-    response = Faraday.post(ENV['BACKEND_URL']/api/v1/users) do |request|
+    response = Faraday.post("#{ENV['BACKEND_URL']}/api/v1/users") do |request|
       request.body = JSON.generate(body)
     end
+    binding.pry
+    user_data = JSON.parse(response.body, symbolize_names: true)
+    user = User.new(user_data)
 
-    user = User.new(response)
-    session[:user] = user
-
-    redirect_to dashboard_index_path
+    # session[:user] = user
+    #
+    # redirect_to dashboard_index_path
   end
 end
