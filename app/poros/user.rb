52ff43fe9c491e4f0233839bd4161c0f36ddf146
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2021-01-09 14:42:39 -0700

    first attempt at bootstrap setup
    Co-authored-by: Shaunda Cunningham <4582791+smcunning@users.noreply.github.com>

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -2,37 +2,9 @@ class User
   attr_reader :id, :name, :image, :friends, :games, :game_nights
 
   def initialize(data)
-<<<<<<< HEAD
-    @id = data[:attributes][:id]
-    @name = data[:attributes][:name]
-    @image = data[:attributes][:image]
-    @friends = []
-    @games = []
-    @game_nights = []
-  end
-
-  def add_friends(friends)
-    friends.each do |friend|
-      @friends << friend
-    end
-  end
-
-  def add_games(games)
-    games.each do |game|
-      @games << game
-    end
-  end
-
-  def add_game_nights(game_nights)
-    game_nights.each do |game_night|
-      @game_nights << game_night
-    end
-  end
-=======
     @id = data[:data][:attributes][:id]
     @name = data[:data][:attributes][:name]
     @image = data[:data][:attributes][:image]
   end
 
->>>>>>> d3b1f114f438bdb0f23768878b8ff8708ca18d1d
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-<<<<<<< HEAD
   resources :dashboard, only: [:index]
-=======
-  get '/auth/google_oauth2', as: :google_login
-  get '/auth/google_oauth2/callback', to: 'sessions#create'
->>>>>>> d3b1f114f438bdb0f23768878b8ff8708ca18d1d
 
   get "/game-nights/new", to: "game_nights#new"
   post "/game-nights", to: "game_nights#create"
   get "/game-nights/:id", to: "game_nights#show"
-<<<<<<< HEAD
 
   resources :games, only: [:new, :create]
   namespace :games do
@@ -24,6 +19,4 @@ Rails.application.routes.draw do
     post '/games/:id', to: 'games#create'
   end
 
-=======
->>>>>>> d3b1f114f438bdb0f23768878b8ff8708ca18d1d
 end
