fe2eb95928b956b2d92ae92644554aaa38ea0a37
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-08 18:54:21 -0700

    Adds user poro
    Co-authored-by: Shaunda Cunningham 
    <4582791+smcunning@users.noreply.github.com>

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -2,21 +2,21 @@ class User
   attr_reader :id, :name, :image, :friends, :games
 
   def initialize(data)
-    @id = data[:attributes][:id]
-    @name = data[:attributes][:name]
-    @image = data[:attributes][:image]
-    @friends = []
+    # @id = data[:attributes][:id]
+    @name = data[:data][:attributes][:name]
+    @image = data[:data][:attributes][:image]
+    # @friends = []
   end
 
-  def add_friends(friends)
-    friends.each do |friend|
-      @friends << friend
-    end
-  end
+  # def add_friends(friends)
+  #   friends.each do |friend|
+  #     @friends << friend
+  #   end
+  # end
+  #
+  # def add_games(games)
+  #   games.each do |game|
+  #     @games << game
+  #   end
 
-  def add_games(games)
-    games.each do |game|
-      @games << game
-    end
-  end
 end
