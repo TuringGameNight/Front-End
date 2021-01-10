d78ee60e107773112f66fb370146cb323e73ef12
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-09 21:37:08 -0700

    Heavily edits user poro

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -1,30 +1,31 @@
 class User
   attr_reader :id, :name, :image, :friends, :games, :game_nights
 
-  def initialize(data)
-    @id = data[:attributes][:id]
-    @name = data[:attributes][:name]
-    @image = data[:attributes][:image]
-    @friends = []
-    @games = []
-    @game_nights = []
+  def initialize(user_info)
+    @id = user_info[:data][:attributes][:id]
+    @name = user_info[:data][:attributes][:name]
+    @image = user_info[:data][:attributes][:image]
+    @friends = user_info[:data][:attributes][:buds]
+    @games = user_info[:data][:attributes][:games]
+    @game_nights = user_info[:data][:attributes][:game_nights]
   end
 
-  def add_friends(friends)
-    friends.each do |friend|
-      @friends << friend
-    end
-  end
-
-  def add_games(games)
-    games.each do |game|
-      @games << game
-    end
-  end
-
-  def add_game_nights(game_nights)
-    game_nights.each do |game_night|
-      @game_nights << game_night
-    end
-  end
+  # def add_friends(friends)
+  #   friends.each do |friend|
+  #     binding.pry
+  #     @friends << friend
+  #   end
+  # end
+  #
+  # def add_games(games)
+  #   games.each do |game|
+  #     @games << game
+  #   end
+  # end
+  #
+  # def add_game_nights(game_nights)
+  #   game_nights.each do |game_night|
+  #     @game_nights << game_night
+  #   end
+  # end
 end
