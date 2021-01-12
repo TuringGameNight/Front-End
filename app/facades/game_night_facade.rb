5f7ab031e55ccff08daf3cda14f6ab412507784c
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-11 20:56:32 -0700

    Add conditional for bad creation
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- app/facades/game_night_facade.rb
+++ app/facades/game_night_facade.rb
@@ -1,12 +1,14 @@
 class GameNightFacade
   def self.create_game_night(data)
     json = GameNightService.create_game_night(data)
-    game_night = GameParty.new(json[:data][:attributes])
-    friends = json[:data][:relationships][:friends][:data].map do |data|
-      Friend.new(data)
+    unless json[:data].empty?
+      game_night = GameParty.new(json[:data][:attributes])
+      friends = json[:data][:relationships][:friends][:data].map do |data|
+        Friend.new(data)
+      end
+      game_night.add_friends(friends)
+      game_night
     end
-    game_night.add_friends(friends)
-    game_night
   end
 
   def self.find_game_night(id)
