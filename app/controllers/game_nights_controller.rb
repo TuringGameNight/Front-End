a427c03ee8d82b9bd32d2b42e218faf7765f6d11
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-12 17:21:55 -0700

    Add instance variables and correct redirect
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- app/controllers/game_nights_controller.rb
+++ app/controllers/game_nights_controller.rb
@@ -3,16 +3,18 @@ class GameNightsController < ApplicationController
 
   def new
     @user = current_user
+    @friends = UserFacade.get_friends(@user.id)
   end
 
   def create
-    game_night = GameNightFacade.create_game_night(params)
-    if game_night
+    game_night = GameNightFacade.create_game_night(params, current_user.id)
+    if game_night[:message] == 'success'
       flash[:success] = 'Game night created successfully!'
-      redirect_to "/game-nights/#{game_night.id}"
+      redirect_to "/dashboard"
     else
       flash.now[:error] = 'Please fill out all required fields.'
       @user = current_user
+      @friends = UserFacade.get_friends(@user.id)
       render :new
     end
   end
@@ -20,5 +22,7 @@ class GameNightsController < ApplicationController
   def show
     @user = current_user
     @game_night = GameNightFacade.find_game_night(params[:id])
+    @friends = UserFacade.get_friends(@user.id)
+    @games = UserFacade.get_games(@user.id)
   end
 end
