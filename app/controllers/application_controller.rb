8b60b0feaf9b97259ac2df88705e3d9c7772efa4
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-12 17:22:20 -0700

    Refactor current_user
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,27 +8,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= create_user(session[:user])
-  end
-
-  private
-
-  def create_user(data)
-    unless data.nil?
-      user = User.new(data)
-      friends = data[:data][:attributes][:buds].map do |friend|
-        Friend.new(friend)
-      end
-      games = data[:data][:attributes][:games].map do |game|
-        Game.new(game)
-      end
-      game_nights = data[:data][:attributes][:game_nights].map do |game_night|
-        GameParty.new(game_night)
-      end
-      user.add_friends(friends)
-      user.add_games(games)
-      user.add_game_nights(game_nights)
-      user
-    end
+    @current_user ||= User.new(session[:user])
   end
 end
