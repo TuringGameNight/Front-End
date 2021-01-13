d6beeabba662f4090e961bcc1760f58c80a8ac91
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-12 17:22:07 -0700

    Add instance variables
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,8 @@ class DashboardController < ApplicationController
 
   def index
     @user = current_user
+    @friends = UserFacade.get_friends(@user.id)
+    @games = UserFacade.get_games(@user.id)
+    @game_nights = UserFacade.get_game_nights(@user.id)
   end
 end
