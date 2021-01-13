e9a754101ae9a05d9106daa961c108970fe972c3
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-12 17:19:07 -0700

    Update user poro
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -5,26 +5,5 @@ class User
     @id = user_info[:data][:attributes][:id]
     @name = user_info[:data][:attributes][:name]
     @image = user_info[:data][:attributes][:image]
-    @friends = []
-    @games = []
-    @game_nights = []
-  end
-
-  def add_friends(friends)
-    friends.each do |friend|
-      @friends << friend
-    end
-  end
-
-  def add_games(games)
-    games.each do |game|
-      @games << game
-    end
-  end
-
-  def add_game_nights(game_nights)
-    game_nights.each do |game_night|
-      @game_nights << game_night
-    end
   end
 end
