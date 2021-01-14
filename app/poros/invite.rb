316d1704267324389c7b65b86e5214bcccf8d559
Grant Dempsey <CryptoDemps@gmail.com>
2021-01-14 14:47:01 -0700

    Add poro to turn game night invitations to objects
    Co-authored-by: Eduardo Parra <56360157+helloeduardo@users.noreply.github.com>

--- app/poros/invite.rb
+++ app/poros/invite.rb
@@ -1,12 +1,15 @@
 class Invite
   attr_reader :id,
+              :host_name,
               :name,
               :date,
               :user_id,
-              :number_of_games
+              :number_of_games,
+              :host_name
 
   def initialize(data)
     @id = data[:id]
+    @host_name = data[:host_name]
     @name = data[:name]
     @date = data[:date]
     @user_id = data[:user_id]
