3dbff3d1ed97638db05cbe71a055a082f060843c
Kate Tester <katemorris@gmail.com>
2021-01-14 14:40:55 -0700

    Fixes issue when user has no games to check

--- app/controllers/users/games_controller.rb
+++ app/controllers/users/games_controller.rb
@@ -20,8 +20,10 @@ class Users::GamesController < ApplicationController
   private
 
   def has_game?(_game_name, session)
-    session[:user][:data][:attributes][:games].any? do |game|
-      game[:name] == (params[:name])
+    if session[:user][:data][:attributes][:games].count > 0
+      session[:user][:data][:attributes][:games].any? do |game|
+        game[:name] == (params[:name])
+      end
     end
   end
 end
